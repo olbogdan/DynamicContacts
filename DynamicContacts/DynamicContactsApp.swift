783785f6e9f2d318f31e07e4a1daed088ee4799b
bogdanov <iceposhta@gmail.com>
2021-04-12 19:28:15 +0100

    Initial Commit

---
+++ DynamicContacts/DynamicContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicContactsApp.swift
+//  DynamicContacts
+//
+//  Created by bogdanov on 12.04.21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
